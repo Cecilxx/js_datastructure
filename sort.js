46589d1c448480a69b0f045fd5e7cf8c9d0c51a8
Cecilxx <xingxing.liu@ele.me>
2017-12-28 15:22:44 +0800

    mergeSort 归并排序

--- sort.js
+++ sort.js
@@ -9,6 +9,39 @@ class ArrayList {
     array[index2] = aux
   }
 
+  static mergeSortRec(array) {
+    if (array.length === 1) return array
+    let mid = Math.floor(array.length / 2)
+    let left = array.slice(0, mid)
+    let right = array.slice(mid, array.length)
+
+    return ArrayList.merge(
+      ArrayList.mergeSortRec(left),
+      ArrayList.mergeSortRec(right)
+    )
+  }
+
+  static merge(left, right) {
+    let result = []
+    let il = 0
+    let ir = 0
+
+    while (il < left.length && ir < right.length) {
+      if (left[il] < right[ir]) {
+        result.push(left[il++])
+      } else {
+        result.push(right[ir++])
+      }
+    }
+    while (il < left.length) {
+      result.push(left[il++])
+    }
+    while (ir < right.length) {
+      result.push(right[ir++])
+    }
+
+    return result
+  }
   insert(item) {
     this.array.push(item)
   }
@@ -17,8 +50,13 @@ class ArrayList {
     return array.join()
   }
 
+  reverse() {
+    this.array.reverse()
+  }
+
   bubbleSort() {
     // 冒泡排序：时间复杂度O(n2)
+    let start = +new Date()
     for (let i = 0; i < this.array.length; i++) {
       for (let j = 0; j < this.array.length - 1 - i; j++) {
         if (this.array[j] > this.array[j + 1]) {
@@ -26,10 +64,13 @@ class ArrayList {
         }
       }
     }
+    let end = +new Date()
+    console.info('bubbleSort:' + (end - start) + 'ms')
   }
 
   selectionSort() {
     // 选择排序：时间复杂度O(n2)
+    let start = +new Date()
     let indexMin = 0
     for (let i = 0; i < this.array.length - 1; i++) {
       indexMin = i
@@ -42,35 +83,46 @@ class ArrayList {
         ArrayList.swap(this.array, i, indexMin)
       }
     }
+    let end = +new Date()
+    console.info('selectionSort:' + (end - start) + 'ms')
   }
 
   insertionSort() {
     // 插入排序：时间复杂度O(n2)，排序小型数组的时候，性能比冒泡和选择好，默认第一项已经排序
+    let start = +new Date()
     let length = this.array.length
     let j = 0
     let temp = null
 
     for (let i = 1; i < length; i++) {
       j = i
-      temp = array[i]
+      temp = this.array[i]
 
-      while (j > 0 && array[j - 1] > array[j]) {
-        array[j] = array[j - 1]
+      while (j > 0 && this.array[j - 1] > temp) {
+        this.array[j] = this.array[j - 1]
         j--
       }
-      array[j] = temp
+      this.array[j] = temp
     }
+    let end = +new Date()
+    console.info('insertionSort:' + (end - start) + 'ms')
+  }
+
+  mergeSort() {
+    let start = +new Date()
+    this.array = ArrayList.mergeSortRec(this.array)
+    let end = +new Date()
+    console.log('mergeSort:' + (end - start) + 'ms')
   }
 }
 
-class CustomHTMLElement {
-  constructor(element) {
-    this.element = element
+function creatArray(length) {
+  let array = new Array(length)
+  let index = 0
+  while (length > 0) {
+    array[index] = length
+    length--
+    index++
   }
-  get html() {
-    return this.element.innerHTML
-  }
-  set html(value) {
-    this.element.innerHTML = value
-  }
+  return array
 }
